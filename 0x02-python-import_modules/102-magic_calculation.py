bccc9a4b7083ec107b57998551dd32faaec542aa
Walterthesmart <nwaugowalter@gmail.com>
2023-05-12 10:17:51 +0100

    Python Import modules

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# function that does exactly the same as the following Python bytecode
+# created by bright
+
+
+def magic_calculation(a, b):
+    """Match bytecode provided by Holberton School."""
+    from magic_calculation_102 import add, sub
+
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return (c)
+
+    else:
+        return(sub(a, b))
---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# program that imports the variable
+# created by bright
+
+if __name__ == "__main__":
+    """Print the value of variable a from variable_load_5."""
+    from variable_load_5 import a
+
+    print(a)
