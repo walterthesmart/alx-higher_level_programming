5a67f0f616834dd0b5c2572db8d609624a61ca93
Walterthesmart <nwaugowalter@gmail.com>
2023-05-30 16:39:50 +0100

    Exceptions

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# function that raises a name exception with a message.
+# created by bright
+
+
+def raise_exception_msg(message=""):
+    """Raise a NameError exception with a message."""
+    raise NameError(message)
